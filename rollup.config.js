b748922744002d4d57abbb2d819573ec297b450a
YashTotale <totaleyash@gmail.com>
2020-11-24 12:12:53 -0800

    added .eslintignore

--- rollup.config.js
+++ rollup.config.js
@@ -10,23 +10,26 @@ const globals = {
 
 const extensions = [".js", ".ts", ".jsx", ".tsx"];
 
-const config = {
-  input: "src/index.tsx",
-  output: [
-    {
-      dir: "dist",
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-      strict: true,
-    },
-  ],
-  plugins: [
-    babel({ exclude: "node_modules/**", extensions }),
-    typescript(),
-    terser(),
-  ],
-  external: Object.keys(globals),
-};
+const defaultPlugins = [
+  babel({ exclude: "node_modules/**", extensions }),
+  terser(),
+];
+
+const config = [
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        dir: "dist",
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        strict: true,
+      },
+    ],
+    plugins: [...defaultPlugins, typescript()],
+    external: Object.keys(globals),
+  },
+];
 
 export default config;
