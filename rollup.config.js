c0afbd4bdcafbec80f8c5d58c46dd898ebbefd68
YashTotale <totaleyash@gmail.com>
2020-11-25 18:51:10 -0800

    changed rollup build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import pkg from "./package.json";
+
 import typescript from "@rollup/plugin-typescript";
 import babel from "@rollup/plugin-babel";
 import replace from "@rollup/plugin-replace";
@@ -11,40 +13,42 @@ const globals = {
 
 const extensions = [".js", ".ts", ".jsx", ".tsx"];
 
-const defaultTS = {
-  declaration: true,
-  noEmitOnError: true,
-};
-
 const cjs = [
   {
     input: "src/index.tsx",
     output: {
       dir: "cjs",
       format: "cjs",
-      exports: "named",
       sourcemap: true,
-      entryFileNames: "react-hash-scroll.js",
+      esModule: false,
+      entryFileNames: `${pkg.name}.js`,
     },
     plugins: [
       replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
-      babel({ exclude: "node_modules/**", sourceMaps: true, extensions }),
-      typescript({ ...defaultTS, declarationDir: "cjs" }),
+      babel({
+        exclude: /node_modules/,
+        sourceMaps: true,
+        rootMode: "upward",
+      }),
+      typescript({ declaration: true, declarationDir: "cjs" }),
     ],
     external: Object.keys(globals),
   },
   {
     input: "src/index.tsx",
     output: {
-      file: "cjs/react-hash-scroll.min.js",
+      file: `cjs/${pkg.name}.min.js`,
       format: "cjs",
-      exports: "named",
       sourcemap: true,
     },
     plugins: [
       replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
-      babel({ exclude: "node_modules/**", sourceMaps: true, extensions }),
-      typescript({ ...defaultTS, declaration: false }),
+      babel({
+        exclude: /node_modules/,
+        sourceMaps: true,
+        rootMode: "upward",
+      }),
+      typescript(),
       terser(),
     ],
     external: Object.keys(globals),
