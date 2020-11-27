c95728ea404d07a32d0fb43ed5530869a5abd492
YashTotale <totaleyash@gmail.com>
2020-11-26 22:07:12 -0800

    Added ChildrenHash component

---
+++ src/Components/ChildrenHash.tsx
@@ -0,0 +1,60 @@
+// React Imports
+import React, { FC, Fragment, ReactElement } from "react";
+import { BaseScrollOptions } from "../Utils/types";
+import HashScroll from "./HashScroll";
+
+type ChildHash = string | (Partial<BaseScrollOptions> & { hash: string });
+
+interface ChildrenHashProps extends BaseScrollOptions {
+  hashes: ChildHash[];
+  children: ReactElement[];
+}
+
+const createReffed = (
+  child: ReactElement,
+  defaultOptions: Partial<BaseScrollOptions>,
+  hash?: ChildHash
+) => {
+  if (!hash) return child;
+  if (typeof hash === "string")
+    return (
+      <HashScroll {...defaultOptions} hash={hash}>
+        {child}
+      </HashScroll>
+    );
+  return (
+    <HashScroll {...defaultOptions} {...hash}>
+      {child}
+    </HashScroll>
+  );
+};
+
+const ChildrenHash: FC<ChildrenHashProps> = ({
+  hashes,
+  children,
+  behavior,
+  position,
+  requiredPathname,
+  scrollFunc,
+}) => {
+  return (
+    <>
+      {children.map((child, i) => (
+        <Fragment key={i}>
+          {createReffed(
+            child,
+            {
+              behavior,
+              position,
+              requiredPathname,
+              scrollFunc,
+            },
+            hashes[i]
+          )}
+        </Fragment>
+      ))}
+    </>
+  );
+};
+
+export default ChildrenHash;
