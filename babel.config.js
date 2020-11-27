c2cd2d9f6b2a7a403c6282a0e9a710e4f46e06e2
YashTotale <totaleyash@gmail.com>
2020-11-27 00:18:13 -0800

    Updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-    { isTSX: true, allExtensions: true },
+    ["@babel/preset-typescript", { isTSX: true, allExtensions: true }],
     "@babel/preset-react",
   ],
 };
