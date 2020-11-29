28b292606b50fd97faff9ef4d377405f2db9a75f
YashTotale <totaleyash@gmail.com>
2020-11-28 19:01:52 -0800

    precommit hook builds README

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,14 +1,22 @@
 import { promisify } from "util";
 import { promises } from "fs";
-import { join } from "path";
+import { join, parse } from "path";
 import { exec } from "child_process";
 import * as moment from "moment";
 
-export const { readFile } = promises;
+export const { readFile, writeFile, readdir: readDir } = promises;
 
 export const execute = promisify(exec);
 
-export { join };
+export { join, parse };
+
+export const getChangedDirs = () => {
+  return execute("git diff --dirstat=files,0 HEAD | sed -E 's/^[ 0-9.]+% //g'");
+};
+
+export const getChangedFiles = () => {
+  return execute("git diff --name-only");
+};
 
 export const today = moment().format("YYYY-MM-DD");
 export const PACKAGE_NAME = "react-hash-scroll";
